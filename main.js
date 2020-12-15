9567c6bdcf7ad293b77440e85297131eaa05c408
ToscaGitHub <72005935+ToscaGitHub@users.noreply.github.com>
2020-12-15 16:42:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
-function changeImage() {
-    document.getElementById("well").src="/image/well light.png";
+var player;
+var timestamp;
 
+function preloadImage (done, i) {
+    if (!i) { i = 1; }
+    
+    
+    var img = new Image();
+    img.onloadend = function () {
+        if (this.height == 0) { return done(); }
+        preloadImage(done, i + 1);
+    };
+    img.src = "image/well/" + i;
 }
 
-function resetImg() {
-    document.getElementById("well").src="/image/well.png";
-
+function start(){
+    clearInterval(player);
+    document.getElementById("well").src = "/image/well/0.png";
+    player = setInterval(function(){ next(); }, 100);
 }
+
+function next(){
+    
+    // while (n<3) {
+        
+        var fullPath=document.getElementById("well").src;
+        var filename = fullPath.split("/").pop();
+        var n=parseInt(filename, 10) + 1;
+        if (n < 6) {
+            document.getElementById("well").src = "image/well/" + n + ".png";
+            if (document.getElementById("well").height == 0) { clearInterval(player); };
+        }
+        if (n == 3) { timestamp = new Date().getTime(); };
+    
+    // }
+}
+
+
+
+
+// usage
+preloadImage(function () { console.log('images loaded'); });
